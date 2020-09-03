bc48ec044c987c2b8d94fbf04e2500e647094cba
ChristianLievrouw <celievrouw1@gmail.com>
2020-09-02 20:29:45 -0500

    started UI

---
+++ LibraryTests/UnitTest1.cs
@@ -0,0 +1,45 @@
+using System;
+using UnitTesting;
+using Xunit;
+
+namespace LibraryTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Library_Tests()
+        {
+            Library<string> library = new Library<string>();
+
+            Assert.Empty(library);
+        }
+
+        [Fact]
+        public void Can_add_to_library()
+        {
+            Library<string> library2 = new Library<string>();
+
+
+            library2.Add("Hi");
+            library2.Add("how");
+            library2.Add("are");
+            library2.Add("you");
+
+            Assert.Equal(new[] { "Hi", "how", "are", "you"}, library2);
+        }
+
+        [Fact]
+        public void Library_can_remove()
+        {
+            // Arange
+            Library<string> removeLibrary = new Library<string>();
+            removeLibrary.Add("book");
+            removeLibrary.Add("GoT");
+            // Act
+            bool result = removeLibrary.RemoveBook(0);
+            // Assert
+            Assert.True(result);
+            Assert.Equal(new[] { "GoT" }, removeLibrary);
+        }
+    }
+}
--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace UnitTesting
 {
     class Program
     {
+        public static Library<Book> Library { get; set; }
+        public static Library<Book> BookBag { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Library = new Library<Book>();
+            BookBag = new Library<Book>();
+
+            LoadBooks();
+        }
+
+        static void LoadBooks()
+        {
+            AddABook("That", "Me", "You", 400, Genre.Comic);
+            AddABook("F******", "Me", "You", 400, Genre.Comic);
+            AddABook("B****", "Me", "You", 300, Genre.Fantasy);
+            AddABook("Carole", "Me", "You", 200, Genre.Horror);
+            AddABook("Baskin", "Me", "You", 100, Genre.Mystery);
+            int counter = 1;
+            foreach (Book book in Library)
+            {
+                Console.WriteLine($"{counter++} : {book.Title}, {book.Author.FirstName}, {book.Author.LastName}, {book.NumberOfPages} {book.Genre}");  
+            }
+        }
+
+        static void AddABook(string title, string firstName, string lastName, int numberOfPages, Genre genre)
+        {
+            Book book = new Book()
+            {
+                Title = title,
+                Author = new Author(firstName, lastName)
+                {
+                    FirstName = firstName,
+                    LastName = lastName
+                },
+                NumberOfPages = numberOfPages,
+                Genre = genre
+            };
+            Library.Add(book);
         }
     }
 }
